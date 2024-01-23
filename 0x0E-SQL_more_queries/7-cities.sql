71f6253c5c0832d8aa2f38bf311188cf22ec159f
Shedie254 <shadrackmuchiri43@gmail.com>
2024-01-23 05:23:30 -0800

    correction on 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
---create adtab'ase hbtn_0d_usa'
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREAT TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-        id INT UNIQUE AUTO_INCREMENT NOT NULL,
-        state_id INT NOT NULL,
-        name VARCHAR(256) NOT NULL,
-        PRIMARY KEY (id);
-        FOREIGN KEY (state_id) REFERENCES STATES(id));
+-- Script that creates a database and a table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; -- Query to create database
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities ( -- Query to create cities in hbtn_0d_usa
+       id INT UNIQUE AUTO_INCREMENT NOT NULL,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY (id),
+       FOREIGN KEY (state_id) REFERENCES states(id));
