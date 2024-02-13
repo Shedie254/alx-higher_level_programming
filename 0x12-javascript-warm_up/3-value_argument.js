6b0931cfc3236552151ea783fd4a482bdc9def32
Shedie254 <shadrackmuchiri43@gmail.com>
2024-02-13 17:37:13 +0000

    debug 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined ){
+const arg = process.argv[2];
+if ( arg  === undefined ){
 	console.log("No argument");
 }
 else {
-	console.log(process.argv[2]);
+	console.log(arg);
 }
