bbe6b08367bf784d0c75e0ed7a2cb10871c92b91
Shedie254 <shadrackmuchiri43@gmail.com>
2024-02-13 17:23:03 +0000

    debug 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
-#!/usr/bin/node
-const argc = process.argv.length;
-if ( argc === 2){
-	console.log("No argument");
-}
-else if ( argc  === 3){
-	console.log("Argument found");
-else {
-	console.log("Arguments found");
+t argc = process.argv.length;
+if (argc === 2) {
+  console.log('No argument');
+} else if (argc === 3) {
+  console.log('Argument found');
+} else if (argc > 3) {
+  console.log('Arguments found');
 }
